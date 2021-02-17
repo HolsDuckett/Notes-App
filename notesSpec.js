e35a25befaf900db9c37126089da544f0d9f2e3e
jshields123 <jackshields@live.co.uk>
2021-02-17 16:26:20 +0000

    added in noteslist and abbreviate and createnote interface

---
+++ notesSpec.js
@@ -0,0 +1,15 @@
+
+
+it('enables person to add note to list', () => {
+  notesList.push("note")
+  expect(notesList[0]).toEqual("note")
+})
+
+
+
+
+
+
+// it('passes equal to', () => {
+//   expect(2).toEqual(2);
+// });

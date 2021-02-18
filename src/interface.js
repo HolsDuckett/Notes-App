0947bf77f9f731e12312e1b9c0cd7371a26d5130
HolsDuckett <hf.duckett@gmail.com>
2021-02-18 15:26:00 +0000

    created updateList and reload notes each time you refresh browser

--- src/interface.js
+++ src/interface.js
@@ -1,17 +1,23 @@
 function createNote() {
 
   notesList.push(document.getElementById('note-input').value)
-  var notes = document.getElementById('all-notes')
-  let tempString = ''
-  for (let i = 0; i < notesList.length; i++) {
-    tempString += `<p>${notesList[i]}</p>`
-  }
-  notes.innerHTML = tempString
-  console.log(notesList);
-
+  updateList()
 }
 
 
 var createButton = document.getElementById('create-button')
 console.log(createButton)
 createButton.addEventListener("click", createNote)
+
+
+function updateList() {
+  var notes = document.getElementById('all-notes')
+  let tempString = ''
+  for (let i = 0; i < notesList.length; i++) {
+    tempString += `<div id="note-${i}" class="notes"><p>${notesList[i]}</p></div>`
+  }
+  notes.innerHTML = tempString
+
+}
+
+updateList()

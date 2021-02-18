88345380cd60c0e1e068a20cad66e45c9770636a
jshields123 <jackshields@live.co.uk>
2021-02-18 16:36:32 +0000

    added text wrap and expand functionality added

--- spec/notesSpec.js
+++ spec/notesSpec.js
@@ -5,7 +5,10 @@ it('enables person to add note to list', () => {
   expect(notesList[0]).toEqual("note")
 })
 
-
+it('it abbreviates to the first 20 characters', () => {
+  string = '123456789012345678901234567890'
+  expect(abbreviate(string)).toEqual('12345678901234567890')
+})
 
 
 
--- src/interface.js
+++ src/interface.js
@@ -9,22 +9,28 @@ var createButton = document.getElementById('create-button')
 console.log(createButton)
 createButton.addEventListener("click", createNote)
 
+function expandNote(index){
+  noteText = document.getElementById(`note-text-${index}`)
+  noteText.style.color = 'red';
+  noteText.textContent = notesList[index]
+}
 
 function updateList() {
   var notes = document.getElementById('all-notes')
-  let tempString = ''
+  let tempHTML = ''
   for (let i = 0; i < notesList.length; i++) {
-    tempString += noteLayout(notesList[i],i)
+    tempHTML += noteLayout(notesList[i],i)
 
   }
-  notes.innerHTML = tempString
+  notes.innerHTML = tempHTML
 
 }
 
 function noteLayout(string, index){
-  return `<div id="note-${index}" class="notes"><p>${abbreviate(string)}</p>
+  return `<div id="note-${index}" class="notes"><p id='note-text-${index}'>${abbreviate(string)}</p>
   <button class="minimize-button" id="minimize-${index}">-</button>
-  <button class="expand-button" id="expand-${index}">+</button></div>`
+  <button class="expand-button" onClick="expandNote(${index})" id="expand-${index}">
+  +</button></div>`
 
 }
 
